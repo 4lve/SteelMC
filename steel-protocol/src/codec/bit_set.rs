3349cbd8695d5a8d0a721f0526227f6949756eff
teknostom <evaldsson.tom@gmail.com>
2025-11-04 23:52:36 +0100

    review fixes

--- steel-protocol/src/codec/bit_set.rs
+++ steel-protocol/src/codec/bit_set.rs
@@ -8,9 +8,9 @@ pub struct BitSet(pub Box<[i64]>);
 
 impl ReadFrom for BitSet {
     fn read(read: &mut impl Read) -> Result<Self, Error> {
-        let length = VarInt::read(read)?;
-        let mut array = Vec::with_capacity(length.0 as usize);
-        for _ in 0..length.0 {
+        let len = VarInt::read(read)?;
+        let mut array = Vec::with_capacity(len.0 as usize);
+        for _ in 0..len.0 {
             array.push(i64::read(read)?);
         }
         Ok(Self(array.into_boxed_slice()))
@@ -19,13 +19,7 @@ impl ReadFrom for BitSet {
 
 impl WriteTo for BitSet {
     fn write(&self, writer: &mut impl Write) -> Result<(), Error> {
-        VarInt(
-            self.0
-                .len()
-                .try_into()
-                .map_err(|_| Error::other("BitSet length not representable as VarInt"))?,
-        )
-        .write(writer)?;
+        VarInt(self.0.len() as i32).write(writer)?;
 
         for &long in self.0.iter() {
             long.write(writer)?;
