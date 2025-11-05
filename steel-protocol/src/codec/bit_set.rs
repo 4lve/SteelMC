301fcbf9a0254e5d7ee4510e6da65efa62bf8e1c
teknostom <evaldsson.tom@gmail.com>
2025-11-05 08:55:43 +0100

    fix comment, reduce reimplementation

--- steel-protocol/src/codec/bit_set.rs
+++ steel-protocol/src/codec/bit_set.rs
@@ -1,28 +1,19 @@
-use std::io::{Error, Read, Write};
+use std::io::{Read, Write};
 
-use crate::packet_traits::{ReadFrom, WriteTo};
+use crate::packet_traits::{PrefixedRead, ReadFrom, WriteTo};
 
 use super::VarInt;
 
 pub struct BitSet(pub Box<[i64]>);
 
 impl ReadFrom for BitSet {
-    fn read(read: &mut impl Read) -> Result<Self, Error> {
-        let len = VarInt::read(read)?;
-        let mut array = Vec::with_capacity(len.0 as usize);
-        for _ in 0..len.0 {
-            array.push(i64::read(read)?);
-        }
-        Ok(Self(array.into_boxed_slice()))
+    fn read(data: &mut impl Read) -> Result<Self, std::io::Error> {
+        Ok(Self(Vec::read_prefixed::<VarInt>(data)?.into_boxed_slice()))
     }
 }
 
 impl WriteTo for BitSet {
-    fn write(&self, writer: &mut impl Write) -> Result<(), Error> {
-        VarInt(self.0.len() as i32).write(writer)?;
-        for val in &self.0 {
-            val.write(writer)?;
-        }
-        Ok(())
+    fn write(&self, writer: &mut impl Write) -> Result<(), std::io::Error> {
+        self.0.write_prefixed::<VarInt>(writer)
     }
 }
